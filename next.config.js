e617124899ede1911818b80419a3145e3ce8441e
Fernando Dingler <ferdingler@users.noreply.github.com>
2023-10-17 15:31:22 -0700

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -3,6 +3,7 @@ const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
   images: {
+    formats: ['image/avif', 'image/webp'],
     remotePatterns: [
       {
         hostname: "**.unsplash.com"

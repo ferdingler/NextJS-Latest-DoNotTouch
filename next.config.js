1c328f6949a2680e668d6d0e2fd336a10074f3d8
Fernando Dingler <ferdingler@users.noreply.github.com>
2023-11-01 17:20:20 -0700

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -3,7 +3,8 @@ const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
   images: {
-    formats: ['image/avif', 'image/webp'],
+    formats: ['image/avif', 'image/webp', 'image/png', 'image/jpg', 'image/jpeg'],
+    sizes: [128, 256, 512, 1024, 2048, 4096],
     minimumCacheTTL: 111,
     remotePatterns: [
       {
